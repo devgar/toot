c1f920df2b8fc8b90c8b3e936ea3f1b51525ebb8
Edgar Albalate <dev.gardo@gmail.com>
2023-02-05 18:32:42 +0100

    Implements initial elefren fetching

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use dotenvy::{dotenv, var};
+
+use std::error::Error;
+
+use elefren::prelude::*;
+
+fn main() ->  Result<(), Box<dyn Error>>{
+    dotenv().ok();
+
+    let mastodon = Mastodon::from(Data{
+        base:  var("APP_BASE").expect("Failed getting app base").into(),
+        client_id: var("APP_ID").expect("Failed getting app Id").into(),
+        client_secret: var("APP_SECRET").expect("Failed getting app secret").into(),
+        redirect: "/".into(),
+        token: var("APP_TOKEN").expect("Failed getting app token").into(),
+
+    });
+        
+    let you = mastodon.verify_credentials()?;
+
+    println!("{:#?}", you);
+
+    Ok(())
 }
