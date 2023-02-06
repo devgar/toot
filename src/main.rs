9b0345943eca901340fd2d769fcdd4d78eee6c44
Edgar Albalate <dev.gardo@gmail.com>
2023-02-06 19:32:17 +0100

    Implements just 'hello world' toot

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,25 @@
+use std::error::Error;
+
 use dotenvy::{dotenv, var};
-use megalodon::{entities, error, generator, SNS};
+use megalodon::{generator, SNS, Megalodon};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn Error>> {
     dotenv().ok();
     // env_logger::init();
 
-    let Ok(url) = var("APP_BASE") else {
-        println!("Specify MASTODON_URL!!");
-        return
-    };
-    let Ok(token) = var("APP_TOKEN") else {
-        println!("Specify MASTODON_ACCESS_TOKEN!!");
-        return
-    };
-    match verify_credentials(url.as_str(), token).await {
-        Ok(response) => {
-            println!("{response:#?}");
-        }
-        Err(err) => {
-            println!("{err:#?}");
-        }
-    }
+    let url = var("APP_BASE")
+        .expect("APP_BASE env variable should be defined");
+    let token = var("APP_TOKEN")
+        .expect("APP_TOKEN env variable should be defined");
+    // let response = verify_credentials(url.as_str(), token).await?;
+
+    let client = gen_client(url, token);
+    let response = client.post_status("Hello, world!".into(), None).await?;
+    println!("{response:#?}");
+    Ok(())
 }
-async fn verify_credentials(
-    url: &str,
-    access_token: String,
-) -> Result<entities::Account, error::Error> {
-    let client = generator(SNS::Mastodon, url.to_string(), Some(access_token), None);
-    let res = client.verify_account_credentials().await?;
-    Ok(res.json())
+
+fn gen_client(url: String, token: String) -> Box<dyn Megalodon + Send + Sync> {
+    generator(SNS::Mastodon, url, Some(token), None)
 }
