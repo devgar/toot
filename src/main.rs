ad484a3971fadae4669dc760af4319bdab9ab2dd
Edgar Albalate <ealbalate@itbacking.com>
2023-02-06 17:29:18 +0100

    Init

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,33 @@
-use std::error::Error;
-
 use dotenvy::{dotenv, var};
-use elefren::{Data, Mastodon, MastodonClient};
+use megalodon::{entities, error, generator, SNS};
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[tokio::main]
+async fn main() {
     dotenv().ok();
+    // env_logger::init();
 
-    let mastodon = Mastodon::from(Data {
-        base: var("APP_BASE").expect("Failed getting app base").into(),
-        client_id: var("APP_ID").expect("Failed getting app Id").into(),
-        client_secret: var("APP_SECRET").expect("Failed getting app secret").into(),
-        redirect: "/".into(),
-        token: var("APP_TOKEN").expect("Failed getting app token").into(),
-    });
-
-    let you = mastodon.verify_credentials()?;
-
-    println!("{you:#?}");
-
-    Ok(())
+    let Ok(url) = var("APP_BASE") else {
+        println!("Specify MASTODON_URL!!");
+        return
+    };
+    let Ok(token) = var("APP_TOKEN") else {
+        println!("Specify MASTODON_ACCESS_TOKEN!!");
+        return
+    };
+    match verify_credentials(url.as_str(), token).await {
+        Ok(response) => {
+            println!("{:#?}", response);
+        }
+        Err(err) => {
+            println!("{:#?}", err);
+        }
+    }
+}
+async fn verify_credentials(
+    url: &str,
+    access_token: String,
+) -> Result<entities::Account, error::Error> {
+    let client = generator(SNS::Mastodon, url.to_string(), Some(access_token), None);
+    let res = client.verify_account_credentials().await?;
+    Ok(res.json())
 }
