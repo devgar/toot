47aff96c6c755b0b5c368ce3cf4e3059ab245d42
Edgar Albalate <ealbalate@itbacking.com>
2023-02-07 12:20:24 +0100

    Implements anyhow for simpler error management

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,13 @@
-use std::error::Error;
-
+use anyhow::Result;
 use dotenvy::{dotenv, var};
-use megalodon::{generator, SNS, Megalodon};
+use megalodon::{generator, Megalodon, SNS};
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() -> Result<()> {
     dotenv().ok();
-    // env_logger::init();
 
-    let url = var("APP_BASE")
-        .expect("APP_BASE env variable should be defined");
-    let token = var("APP_TOKEN")
-        .expect("APP_TOKEN env variable should be defined");
-    // let response = verify_credentials(url.as_str(), token).await?;
+    let url = var("APP_BASE").expect("APP_BASE env variable should be defined");
+    let token = var("APP_TOKEN").expect("APP_TOKEN env variable should be defined");
 
     let client = gen_client(url, token);
     let response = client.post_status("Hello, world!".into(), None).await?;
